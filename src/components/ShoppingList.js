d1675c9643a2b3a12bc1ea5ccb2eefbd3f351d95
Elijah Smith <elijahsmith.m@gmail.com>
2022-04-06 10:06:29 -0500

    submitting with all features (I would like to come back and see if I can make the filter not delete the things that were selected after you choose a new category)

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelect] = useState('Filter by category')
+  console.log(items)
+
+  function handleFilter(e) {
+    setSelect(e.target.value)
+  }
+  const foodsToDisplay = items.filter(item => {
+    if (selectedCategory === 'Filter by category') {
+      return true
+    } else {
+      return item.category === selectedCategory
+    }
+  })
+
+  const foodList = foodsToDisplay.map((item) => (
+    <Item key={item.id} name={item.name} category={item.category} />
+  ))
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={(e) => handleFilter(e)} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +31,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {foodList}
       </ul>
     </div>
   );
